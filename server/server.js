20c832562f88365f012ac6d36d425b4a0fabd30d
technicalsupport3 <akashmahto1169@gmail.com>
2025-10-10 23:30:56 +0530

    setup the server

---
+++ server/server.js
@@ -0,0 +1,20 @@
+import express from "express"
+import cors from "cors";
+import 'dotenv/config';
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+
+app.get('/', (req, res) => {
+    res.send('Server is Live Now')
+})
+
+const PORT = process.env.PORT || 7000
+
+
+app.listen(PORT, () => {
+    console.log(`Srevre is running at http://localhost:${PORT}`)
+})
